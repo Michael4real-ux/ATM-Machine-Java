de4e977935320e69209e7b3bb33aa8031e0b0948
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2023-01-10 10:04:07 +0100

    Atm Entities, getters and setters method added

--- src/main/java/org/atm/ATM.java
+++ src/main/java/org/atm/ATM.java
@@ -1,4 +1,41 @@
 package org.atm;
 
 public class ATM {
+    private double balance;
+    private double depositAmount;
+    private double withdrawAmount;
+
+    //Default constructor
+    public ATM(){
+        balance = 0.00;
+        depositAmount =0.00;
+        withdrawAmount =0.00;
+    }
+
+    // getter and setter
+    public double getBalance(){
+        return balance;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    public double getDepositAmount(){
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount){
+        this.depositAmount = depositAmount;
+    }
+
+    public double getWithdrawAmount(){
+        return withdrawAmount;
+    }
+    public void setWithdrawAmount(double withdrawAmount){
+        this.withdrawAmount = withdrawAmount;
+    }
+
+
+
 }
